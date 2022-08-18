483a17818e269c2f08fa77e42d810008ef74f53a
leobenevids <leonardobenevides@unifor.br>
2022-08-18 17:33:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const initialValues = { token: "", balance: "" };
+  const [formData, setFormData] = useState(initialValues);
+
+  const items = [];
+
+  const storeData = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  const getData = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    getData("items");
+    if ((items.length = 0)) {
+      items.push(formData);
+    } else {
+      items.concat(formData);
+    }
+    console.log(formData);
+
+    storeData("items", items);
+
+    setFormData(initialValues);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Token
+          <input
+            type="text"
+            placeholder="type the Token's name"
+            name="token"
+            value={formData.token}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Balance
+          <input
+            type="number"
+            placeholder="type the balance"
+            name="balance"
+            value={formData.balance}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
